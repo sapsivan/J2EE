d4aba2f9dddb74b4f368b8c16fe9eefd26a89d02
Sivanandan Sankaraswami <sivandev@gmail.com>
2026-03-29 18:26:07 +0530

    Add circuit breaker - Resilience4j - test fails

--- week4/order-service/src/main/java/com/example/demo/order/OrderService.java
+++ week4/order-service/src/main/java/com/example/demo/order/OrderService.java
@@ -2,6 +2,7 @@ package com.example.demo.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @Service
 @RequiredArgsConstructor
@@ -9,6 +10,7 @@ public class OrderService {
 
     private final InventoryClient inventoryClient;
 
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallback")
     public String placeOrder(Long productId) {
 
         boolean available = inventoryClient.isAvailable(productId);
@@ -18,5 +20,9 @@ public class OrderService {
         }
 
         return "ORDER CREATED";
+    }
+
+    public String fallback(Long productId, Throwable throwable) {
+        return "REJECTED - SERVICE UNAVAILABLE";
     }
 }
